e0e4f660c4dd51f55f45c30d420680aeb735badd
Tequila <zl@ZLdeMac.local>
2016-06-24 13:28:04 +0800

    about me update

--- SchoolChat/AboutMeViewController.swift
+++ SchoolChat/AboutMeViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class AboutMeViewController: UIViewController{
+class AboutMeViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    @IBOutlet weak var info: UITableView!
+    
+
     //退出按钮
     
-    @IBAction func signout(sender: AnyObject) {
+    /*@IBAction func signout(sender: AnyObject) {
         
         //清空用户账户信息
         let CurrentAccount = NSUserDefaults.standardUserDefaults()
@@ -26,21 +29,12 @@ class AboutMeViewController: UIViewController{
         appDelegate.window?.rootViewController = LoginVC
         
         
-    }
+    }*/
     
     
     //@IBOutlet weak var MyTabView: UITableView!
     
-    func initInfoCell (image: UIImage, ID:String, name:String)-> UITableViewCell
-    {
-        
-        
-        let infocell = UITableViewCell.init()
-        let imageview = UIImageView.init(image: image)
-        infocell.addSubview(imageview)
-        
-        return infocell
-    }
+
     
 
  
@@ -48,26 +42,162 @@ class AboutMeViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let currentUser = NIMSDK.sharedSDK().userManager.userInfo(NIMSDK.sharedSDK().loginManager.currentAccount())
-        let avatar = UIImage(named: "avatar_user")
-        let accid = currentUser?.userId
-        var showName = ""
-        if(currentUser?.userInfo?.nickName == nil)
-        { showName = accid!}
-        else
-        { showName = (currentUser?.userInfo?.nickName)!}
-        
-        initInfoCell(avatar!, ID: accid!, name: showName)
+        info.backgroundColor = UIColor.init(white: 0.95, alpha: 1)
+     
+      
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
     
 
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (section == 0)
+        {return 1}
+        else if (section == 1)
+        {return 2}
+        else if (section == 2)
+        {return 3}
+        else
+        {return 1}
+    }
 
-
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if (section == 2)
+        {return 50}
+        else if(section == 3)
+        {return 20 }
+        else{return 0}
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell : UITableViewCell
+        let user = NIMSDK.sharedSDK().userManager.userInfo(NIMSDK.sharedSDK().loginManager.currentAccount())
+        let image : UIImage
+        if(indexPath.section == 0 )
+        {
+            cell = UITableViewCell.init(style: .Default, reuseIdentifier: "Avatar")
+            cell.textLabel?.textColor = UIColor.blackColor()
+            cell.textLabel?.text = "头像"
+            cell.frame = CGRectMake(0, 0, cell.frame.size.width, cell.frame.size.height + 40)
+            
+            if(user?.userInfo?.avatarUrl != nil)
+                {         image = UIImage.init(CIImage: CIImage.init(contentsOfURL: NSURL.init(string: (user?.userInfo?.avatarUrl)!)! )!)            }
+            else
+                {            image = UIImage(named: "avatar_user")!            }
+            
+            let imageV = UIImageView.init(image: image)
+            
+            
+            
+            NSLayoutConstraint(item: imageV, attribute: NSLayoutAttribute.Width, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 70).active = true
+            NSLayoutConstraint(item: imageV, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1, constant: 70).active = true
+            
+            let RightConstraint = NSLayoutConstraint.init(item: imageV, attribute:NSLayoutAttribute.Trailing , relatedBy: NSLayoutRelation.Equal , toItem: cell, attribute: NSLayoutAttribute.Trailing , multiplier: 1, constant: -40)
+            let CenterConstraint = NSLayoutConstraint.init(item: imageV, attribute: NSLayoutAttribute.CenterY, relatedBy: NSLayoutRelation.Equal, toItem: cell, attribute: NSLayoutAttribute.CenterY, multiplier: 1, constant: 0)
+            
+            cell.addSubview(imageV)
+            imageV.translatesAutoresizingMaskIntoConstraints = false
+            cell.addConstraint(RightConstraint)
+            cell.addConstraint(CenterConstraint)
+            cell.accessoryType = .DisclosureIndicator
+        }
+            
+        else if(indexPath.section == 1 || indexPath.section == 2)
+        {
+        
+        cell = UITableViewCell.init(style: UITableViewCellStyle.Value1, reuseIdentifier: "infotext")
+        cell.detailTextLabel?.textColor = UIColor.init(white: 0.5, alpha: 1)
+        cell.textLabel?.textColor = UIColor.blackColor()
+        
+            
+            if(indexPath.section == 1)
+            {
+                if (indexPath.row == 0)
+                {
+                    cell.textLabel?.text = "Account"
+                    cell.detailTextLabel?.text = user?.userId
+                }
+                else
+                {
+                cell.textLabel?.text = "Nickname"
+                    if( user!.userInfo?.nickName == nil)
+                    { cell.detailTextLabel?.text = user?.userId }
+                    else
+                    {cell.detailTextLabel?.text = user!.userInfo?.nickName}
+                }
+            }
+            else
+            {
+                if (indexPath.row == 0)
+                {
+                    cell.textLabel?.text = "Sign"
+                    cell.detailTextLabel?.text = ""
+                }
+                else if(indexPath.row == 1)
+                {
+                    cell.textLabel?.text = "Email"
+                    cell.detailTextLabel?.text = ""
+                }
+                else
+                {
+                    cell.textLabel?.text = "Gender"
+                    cell.detailTextLabel?.text = ""
+                
+                }
+            }
+            
+        }
+        
+        else
+        {
+            cell = UITableViewCell.init(style: .Default, reuseIdentifier: "Logout")
+            cell.textLabel?.text = "退出登录"
+            cell.textLabel?.textAlignment = NSTextAlignment.Center
+        }
+        
+        
+        switch (indexPath.section ,indexPath.row) {
+        case (3,0):
+            break
+        case (1,0):
+            break
+        default:
+            cell.accessoryType = .DisclosureIndicator}
+    return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let user = NIMSDK.sharedSDK().userManager.userInfo(NIMSDK.sharedSDK().loginManager.currentAccount())
+        
+        if(indexPath.section == 1 && indexPath.row == 1)
+            {
+            let NicknameVC = UIViewController.init()
+            let textField = UITextField.init(frame: CGRectMake(0, 0, 200, 40))
+            let textView = UIView.init(frame: CGRectMake(0, 0, 200, 40))
+            textField.text = user?.userInfo?.nickName
+            NicknameVC.view.addSubview(textView)
+                
+                self.navigationController?.pushViewController(NicknameVC, animated: true)
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if (indexPath.section == 0)
+        { return 80 }
+        else
+        { return 44 }
+    }
+    
+    
 }
